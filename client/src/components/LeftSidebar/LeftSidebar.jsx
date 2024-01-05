c8d2aa7c6d9f0a8e2bd07c67f62027b97bf010d6
Pratim Bhagat <87892679+HUNTER9769@users.noreply.github.com>
2024-01-05 16:47:09 +0530

    Sidebar update

---
+++ client/src/components/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './LeftSidebar.css'
+import  {NavLink} from 'react-router-dom'
+import Globe from '../../assets/Globe.svg'
+
+const LeftSidebar = () => {
+  return (
+    <div className='left-sidebar'>
+        <nav className='side-nav'>
+            <NavLink to='/' className='side-nav-links' activeClassName ='active' style={{ paddingLeft: '40px'}}>
+                <p>Home</p>
+            </NavLink>
+                <div className='side-nav-div'>
+                    <div><p>PUBLIC</p></div>
+                    <NavLink to='/Questions' className='side-nav-links'>
+                        <img src={Globe} alt="Globe" />
+                        <p style={{paddingLeft:'10px'}}>Questions</p>
+                    </NavLink>
+                </div>
+
+        </nav>
+    </div>
+  )
+}
+
+export default LeftSidebar
---
+++ client/src/components/RightSidebar/RightSidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+    <div>RIghtSidebar</div>
+  )
+}
+
+export default RightSidebar
