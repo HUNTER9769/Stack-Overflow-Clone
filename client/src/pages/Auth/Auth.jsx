21b241aaef84b680d7005a0ed7371c93529ad134
Pratim Bhagat <87892679+HUNTER9769@users.noreply.github.com>
2024-01-12 22:51:57 +0530

    jwt authntication success
    completed login/ signup authentication.

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,57 +1,87 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
 import './Auth.css'
 import icon from '../../assets/icon.png'
 import AboutAuth from './AboutAuth'
+import { signup, login  } from '../../actions/auth'
 const Auth = () => {
 
-  const [isSignUp, setIsSignUp] = useState(false)
-  const handleSwitch = () => {
-    setIsSignUp(!isSignUp)
+    const [isSignup, setIsSignup] = useState(false)
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const handleSwitch = () => {
+    setIsSignup(!isSignup)
   }
+
+  const handleSubmit = (e) => {
+      e.preventDefault()
+      if(!email && !password) {
+        alert('Enter Email and Password to continue.')
+      }
+
+      if(isSignup) {
+        if(!name) {
+            alert('Enter Name to continue.')
+        }
+        dispatch(signup({ name, email, password}, navigate))
+      }else {
+        dispatch(login({ email, password}, navigate))
+      }
+
+    } 
+
+
   return (
-    <section class='auth-section'>
-        { isSignUp && <AboutAuth/>}
-        <div class='auth-container-2'>
-            {!isSignUp &&<img src={icon} alt='stack overflow' className='login-logo'/> }
-            <form >
+    <section className='auth-section'>
+        { isSignup && <AboutAuth/>}
+        <div className='auth-container-2'>
+            {!isSignup &&<img src={icon} alt='stack overflow' className='login-logo'/> }
+            <form onSubmit={handleSubmit} >
                 {
-                    isSignUp && (
+                    isSignup && (
                          <label htmlFor='name'>
                             <h4>Display Name</h4>
-                            <input type="text" id="name" name="name" />
+                            <input type="text" id="name" name="name" onChange={(e) => {setName(e.target.value)} }/>
 
                          </label>
                     )
                 }
                 <label htmlFor='email'>
                     <h4>Email</h4>
-                    <input type="email" name='email' id='email' />
+                    <input type="email" name='email' id='email' onChange={(e) => {setEmail(e.target.value)} }/>
                     <div style={{display: 'flex', justifyContent: 'space-between' }}>
                     <h4>Password</h4>
-                    {!isSignUp && <h4 style={{color:"#007ac6"}}>Forgot Password</h4>}
+                    {!isSignup && <h4 style={{color:"#007ac6"}}>Forgot Password</h4>}
                     </div>
-                    <input type="password" name='passwordS' id='password' />
-                    {isSignUp &&  <p>Password must contain atleast 8<br/> characters, including at least 1
+                    <input type="password" name='passwordS' id='password' onChange={(e) => {setPassword(e.target.value)}} />
+                    {isSignup &&  <p>Password must contain atleast 8<br/> characters, including at least 1
                      <br/>letter and 1 number<br /></p>}
                 </label>
                 {
-                    isSignUp && (
+                    isSignup && (
                         <label htmlFor='check'>
                             <input type="checkbox" id='check'/>
                             <p>Opt-in to receive occasional product updates<br />, user research invitation<br />, company, annoucements and digests<br /></p>
                         </label>
                     )
                 }
-                <button type='submit' className='auth-btn'>{isSignUp ? 'Sign up':' Log in '} </button>
+                <button type='submit' className='auth-btn'>{isSignup ? 'Sign up':' Log in '} </button>
                 {
-                    isSignUp && (
+                    isSignup && (
                         <p style={{color:"#666767", fontSize:"13px"}}>By clicking "Sign Up", you agree to our <span style={{color: "#007ac6"}}>terms of <br /> service</span> our  <span style={{color: "#007ac6"}}>privacy policy</span> and <span style={{color: "#007ac6"}}>cookie policy</span></p>
                     )
                 }
             </form>
             <p>
-                {isSignUp ? 'already have an account?': "Dont have an account ? "}
-                <button type='button' className='handle-switch-button' onClick={handleSwitch}>{ isSignUp ? "Log In "  : "Sign Up" }</button>
+                {isSignup ? 'already have an account?': "Dont have an account ? "}
+                <button type='button' className='handle-switch-button' onClick={handleSwitch}>{ isSignup ? "Log In "  : "Sign Up" }</button>
             </p>
         </div>
 
---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+import userRoutes from './routes/users.js'
+
+const app = express();
+
+app.use(express.json({limit: '30mb', extended: true}))
+app.use(express.urlencoded({limit: '30mb', extended: true}))
+app.use(cors());
+
+app.get('/',(req, res) => {
+    res.send("This is a stack overflow clone API")
+})
+
+app.use('/user', userRoutes)
+
+const PORT = process.env.PORT || 5000; 
+
+const CONNECTION_URL = "mongodb+srv://bhagatpratim21:RjPw5p7Q3lgbsjQn@stack-overflow-cluster.de38rp4.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect( CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => {console.log(`Server is running on port: ${PORT}`)}))
+    .catch((err) => console.log(err.message))
