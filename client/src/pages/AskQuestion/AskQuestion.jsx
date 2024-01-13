5d6ffd59daf8eb424d222850653aa59519d9fbd9
Pratim Bhagat <87892679+HUNTER9769@users.noreply.github.com>
2024-01-14 02:03:23 +0530

    Update askquestion.jsx
    replaced problem created by onclick with onchange. realtime state change successful.

--- client/src/pages/AskQuestion/AskQuestion.jsx
+++ client/src/pages/AskQuestion/AskQuestion.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState} from 'react'
 import './AskQuestion.css'
 
 const AskQuestion = () => {
@@ -9,9 +9,11 @@ const AskQuestion = () => {
     const handleSubmit = (e) => {
         e.preventDefault()
         // let newk = { questionTitle, questionBody, questionTags}
+        console.log(questionTitle)
         console.log({ questionTitle, questionBody, questionTags})
     }
 
+    
     return (
         <div className='ask-question'>
             <div className='ask-ques-container'>
@@ -22,17 +24,17 @@ const AskQuestion = () => {
                         <label htmlFor="ask-ques-title">
                             <h4>Title</h4>
                             <p>Be specific and imagine you're asking a question to another person </p>
-                            <input type="text" id='ask-ques-title' onClick={(e) => {setQuestionTitle(e.target.value)}} placeholder='e.g. Is there an R function for finding the index of an element in a vector'/>
+                            <input type="text" id='ask-ques-title' onChange={(e) => {setQuestionTitle(e.target.value)}} placeholder='e.g. Is there an R function for finding the index of an element in a vector'/>
                         </label>
                         <label htmlFor="ask-ques-body">
                             <h4>Body</h4>
                             <p>Include all the information someone would need to answer your question </p>
-                            <textarea name="" id="ask-ques-body" onClick={(e) => {setQuestionBody(e.target.value) }} cols="30" rows="10"></textarea>
+                            <textarea name="" id="ask-ques-body" onChange={(e) => {setQuestionBody(e.target.value) }} cols="30" rows="10"></textarea>
                         </label>
                         <label htmlFor="ask-ques-tags">
                             <h4>Tags</h4>
                             <p>Add upto 5 tags to describe what your question is about</p>
-                            <input type="text"  id='ask-ques-tags' onClick={(e) => {setQuestionTags(e.target.value.split(" ")) }}  placeholder='e.g. (xml , typescript, wordpress)'/>
+                            <input type="text"  id='ask-ques-tags' onChange={(e) => {setQuestionTags(e.target.value.split(" ")) }}  placeholder='e.g. (xml , typescript, wordpress)'/>
                         </label>
 
                     </div>
