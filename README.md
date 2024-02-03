76852b703c459909c9c702ef15a1794a2e189a75
Pratim Bhagat <bhagatpratim21@gmail.com>
2024-02-03 22:12:37 +0530

    added readme instructions

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # STACK-OVERFLOW-CLONE
+
  STACK OVERFLOW CLONE PROJECT INTERNSHIP TRAINING
+
+
+HOW TO USE APPLICATION
+
+`npm install`
+
+Use above command on client and server side to install all dependencies
+
+`npm start`
+
+use above command to run the server and client
--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
+
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 import userRoutes from './routes/users.js'
 import questionRoutes from './routes/Questions.js'
 import answerRoutes from './routes/Answers.js'
 
 const app = express();
-
+dotenv.config();
 app.use(express.json({limit: '30mb', extended: true}))
 app.use(express.urlencoded({limit: '30mb', extended: true}))
 app.use(cors());
@@ -22,8 +24,8 @@ app.use('/answer', answerRoutes)
 
 const PORT = process.env.PORT || 5000; 
 
-const CONNECTION_URL = "mongodb+srv://bhagatpratim21:RjPw5p7Q3lgbsjQn@stack-overflow-cluster.de38rp4.mongodb.net/?retryWrites=true&w=majority"
+const DATABASE_URL = process.env.CONNECTION_URL 
 
-mongoose.connect( CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect( DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => {console.log(`Server is running on port: ${PORT}`)}))
     .catch((err) => console.log(err.message))
